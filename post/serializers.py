73ae7d8f7ec611675607dcfd15d8a79a9fb0e90d
unknown <adilaralbaev00@gmail.com>
2023-09-14 12:11:51 +0600

    changed serializer

--- post/serializers.py
+++ post/serializers.py
@@ -7,3 +7,9 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         exclude = ['user', ]
         fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
